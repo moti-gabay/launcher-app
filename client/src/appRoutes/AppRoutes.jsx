1c1ac67e7d21628ee7f362496ad76e818f82795c
moti gabay <motigabay18@gmail.com>
2026-03-16 14:18:19 +0200

    pages

---
+++ client/src/appRoutes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Router, Routes } from 'react-router-dom'
+import HomePage from '../pages/HomePage/HomePage'
+import AddLauncherPage from '../pages/AddLauncherPage/AddLauncherPage'
+import LaunchterDatailsPage from '../pages/LaunchterDatailsPage/LaunchterDatailsPage'
+
+function AppRoutes() {
+    return (
+        <Routes>
+
+            <Route path='/' element={<HomePage />} />
+            <Route path='/AddLauncherPage' element={<AddLauncherPage />} />
+            <Route path='/LaunchterDatailsPage' element={<LaunchterDatailsPage />} />
+        </Routes>
+        
+    )
+}
+
+export default AppRoutes
