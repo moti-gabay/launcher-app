eae252a22e76c92872c89f001466f07f8c0acc9a
moti gabay <motigabay18@gmail.com>
2026-03-16 13:58:40 +0200

    routers works

---
+++ client/src/pages/AddLauncherPage/AddLauncherPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddLauncherPage() {
+    return (
+        <div>
+            <h1>AddLauncherPage</h1>
+        </div>
+    )
+}
+
+export default AddLauncherPage
---
+++ client/src/pages/LaunchterDatailsPage/LaunchterDatailsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LaunchterDatailsPage() {
+  return (
+    <div>
+      <h1>LaunchterDatailsPage</h1>
+    </div>
+  )
+}
+
+export default LaunchterDatailsPage
